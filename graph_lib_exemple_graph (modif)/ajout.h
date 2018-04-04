d8f5a6968ce3e9f7d9649148fad698f969946ad0
Unknown <antoine.vidal@edu.ece.fr>
2018-04-04 13:27:22 +0200

    Projet MAJ1
    C'est trop bien on a un code qui nage !!

---
+++ graph_lib_exemple_graph (modif)/ajout.h
@@ -0,0 +1,18 @@
+#ifndef AJOUT_H_INCLUDED
+#define AJOUT_H_INCLUDED
+
+#include "grman/grman.h"
+#include <iostream>
+
+#include "graph.h"
+
+int afficher_menu();
+
+void reseau_1();
+
+void reseau_2();
+
+void reseau_3();
+
+
+#endif // AJOUT_H_INCLUDED
